92f10733c09c1e0388d55fde327768b1345a98ee
Nahum <losond8@gmail.com>
2021-05-29 17:53:54 -0700

    Understanding how to pass functions as arg. Callback functions that simply return functions. Use of basic array methods.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    console.log(array)
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    console.log(array.slice(-2))
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+const createFareMultiplier = function(x){
+    return function(y){
+      return  x * y
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2)
+
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers =(arrayOfDrivers, cb) => {
+    return cb(arrayOfDrivers)
+}
